d72f05e153dd11b7b06699310ccf344a9d444fd2
vojeda <vojeda@set.gov.py>
2022-12-21 14:59:35 -0300

    Se realizan correcciones visuales. Correcciones en las url de las paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'character-list', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) },
-  { path: 'episodes', loadChildren: () => import('./components/pages/episodes/episodes.module').then(m => m.EpisodesModule) },
+  { path: '', redirectTo: 'personajes', pathMatch: 'full' },
+  { path: 'inicio', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'episodios', loadChildren: () => import('./components/pages/episodes/episodes.module').then(m => m.EpisodesModule) },
   {
-    path: 'character-list',
+    path: 'personajes',
     loadChildren: () => import('./components/pages/characters/characters-list/characters-list.module').then(m => m.CharactersListModule)
   },
   {
-    path: 'character-details/:id',
+    path: 'personaje-detalle/:id',
     loadChildren: () => import('./components/pages/characters/characters-details/characters-details.module').then(m => m.CharactersDetailsModule)
   },
-  { path: 'about', loadChildren: () => import('./components/pages/about/about.module').then(m => m.AboutModule) },
+  { path: 'informacion', loadChildren: () => import('./components/pages/about/about.module').then(m => m.AboutModule) },
   { path: '**', loadChildren: () => import('./components/pages/not-found/not-found.module').then(m => m.NotFoundModule) },
 ];
 
